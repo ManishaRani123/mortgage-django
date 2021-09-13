499fc8729ff019f3ab20736e336572ebbdfafd8a
NISHAN <aryalnishan@gmail.com>
2021-09-13 12:27:48 +0545

    disable paypal

--- core/settings.py
+++ core/settings.py
@@ -148,9 +148,9 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-PAYPAL_RECEIVER_EMAIL = 'youremail@mail.com'
+# PAYPAL_RECEIVER_EMAIL = 'youremail@mail.com'
 
-PAYPAL_TEST = True
+# PAYPAL_TEST = True
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
